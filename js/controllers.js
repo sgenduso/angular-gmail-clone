0c1fe081fc7bc37db372f795c0e9be03b0bec147
Sam Genduso <sam.genduso@gmail.com>
2015-09-15 13:16:51 -0600

    select all enables buttons, checks boxes

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,10 @@
 app.controller('InboxController', ['$scope', 'EmailsService', function ($scope, EmailsService) {
+  $scope.allSelected = false;
   EmailsService.getEmails()
   .then(function (emails) {
     $scope.emails = emails;
   });
+  $scope.toggleSelection = function () {
+    $scope.allSelected = !$scope.allSelected;
+  };
 }]);
--- js/services.js
+++ js/services.js
@@ -1,11 +1,15 @@
 app.factory('EmailsService', ['$http', function ($http) {
+  var getEmails = function () {
+    return $http.get('http://localhost:3000/api')
+    .then(function (emails) {
+      console.log(emails.data);
+      return emails.data;
+    });
+  };
+
+
+
   return {
-    getEmails: function () {
-      return $http.get('http://localhost:3000/api')
-      .then(function (emails) {
-        console.log(emails.data);
-        return emails.data;
-      });
-    },
+    getEmails: getEmails,
   };
 }]);
